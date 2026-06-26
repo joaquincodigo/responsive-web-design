6910d1c8c7f3a82f8e9081231544ffe50d86917b
Joaquin.Codigo <joaquin.codigo@gmail.com>
2026-06-26 12:29:09 -0300

    Improved drawer

--- app/components/Drawer.jsx
+++ app/components/Drawer.jsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 import Link from "next/link";
 import { teamIcon, homeIcon, contactIcon } from "@/public/icons/icons";
+import { usePathname } from "next/navigation";
 
 const items = [
   { label: "Home", icon: homeIcon, link: "/home" },
@@ -10,6 +11,9 @@ const items = [
 ];
 
 export default function Drawer({ isOpen, setIsOpen }) {
+
+  const pathname = usePathname();
+
   // Close on ESC key press
   useEffect(() => {
     const handler = (e) => e.key === "Escape" && setIsOpen(false);
@@ -33,18 +37,29 @@ export default function Drawer({ isOpen, setIsOpen }) {
         className={`fixed top-12 left-0 bottom-0 w-56 bg-white shadow-xl z-50 transition-transform duration-300 ease-in-out 
                     ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
       >
-        <nav className="flex flex-col mt-6 ms-3 gap-y-3">
-          {items.map(({ label, icon, link }) => (
-            <Link key={label} href={link}>
-              <button
-                onClick={() => setIsOpen(false)}
-                className="flex items-center gap-x-1.5  w-40 rounded px-3 active:bg-brand-blue active:text-white"
+        <nav className="flex flex-col pt-6 px-3 gap-y-3">
+          {items.map(({ label, icon, link }) => {
+            const isCurrentPage = pathname === link;
+
+            return (
+              <Link
+                key={label}
+                href={link}
+                className={`w-full py-1 ps-3 hover:pointer active:bg-brand-blue active:text-white active:rounded-md ${isCurrentPage
+                  ? "text-brand-blue font-bold bg-brand-blue/10 rounded-md"
+                  : ""
+                  }`}
               >
-                {icon}
-                <span>{label}</span>
-              </button>
-            </Link>
-          ))}
+                <button
+                  onClick={() => setIsOpen(false)}
+                  className="flex items-center gap-x-1.5 w-full h-full"
+                >
+                  {icon}
+                  <span>{label}</span>
+                </button>
+              </Link>
+            );
+          })}
         </nav>
       </div>
     </>
